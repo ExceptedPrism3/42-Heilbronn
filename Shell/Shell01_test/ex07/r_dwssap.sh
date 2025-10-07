46e48acfb24e689c15ba30bfda2f39509c9980b8
Ayoub Ben Cadi <aben-cad@3-B-2.42heilbronn.de>
2025-10-07 19:28:39 +0200

    Fix 3.0

--- Shell/Shell01_test/ex07/r_dwssap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cat /etc/passwd | grep -v '^#' | sed -n 'n;p' \
-| cut -d ':' -f1 \
-| rev \
-| sort -r \
-| awk -v start="$FT_LINE1" -v end="$FT_LINE2" 'NR >= start && NR <= end' \
-| paste -sd ", " - \
-| sed 's/$/./'
