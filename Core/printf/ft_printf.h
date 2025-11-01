2340f0bdd3e3106d44172ecf98559969ac4ad7e4
Ayoub Ben Cadi <aben-cad@3-E-29.42heilbronn.de>
2025-11-02 00:49:24 +0100

    Printf Project Completion

---
+++ Core/printf/ft_printf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aben-cad <aben-cad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 10:23:14 by aben-cad          #+#    #+#             */
+/*   Updated: 2025/10/30 15:47:07 by aben-cad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+int		ft_putchar_fd(char c, int fd);
+int		ft_putstr_fd(char *s, int fd);
+int		ft_putnbr_fd(int n, int fd);
+int		ft_putnbr_x(unsigned long long nbr, char c);
+int		ft_putptr(void *ptr);
+char	*ft_strchr(const char *s, int c);
+
+int		ft_printf(const char *s, ...);
+
+#endif
---
+++ Core/printf/ft_putptr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putptr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aben-cad <aben-cad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 17:08:33 by aben-cad          #+#    #+#             */
+/*   Updated: 2025/11/01 22:13:46 by aben-cad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putptr(void *ptr)
+{
+	unsigned long long	addr;
+	int					count;
+
+	count = 0;
+	if (!ptr)
+		return (ft_putstr_fd("(nil)", 1));
+	addr = (unsigned long long)ptr;
+	count += ft_putstr_fd("0x", 1);
+	count += ft_putnbr_x(addr, 'x');
+	return (count);
+}
