3d5f0fe9558d31d7082b46c114e6646dfe095a11
ExceptedPrism3 <ben.ayoube@outlook.com>
2025-12-08 22:07:47 +0100

    printf projec bonus experiments

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /* By: aben-cad <aben-cad@student.42.fr>          +#+  +:+       +#+        */
 /* +#+#+#+#+#+   +#+           */
 /* Created: 2025/10/27 10:23:14 by aben-cad          #+#    #+#             */
-/* Updated: 2025/12/09 01:00:00 by aben-cad         ###   ########.fr       */
+/* Updated: 2025/12/09 10:00:00 by aben-cad         ###   ########.fr       */
 /* */
 /* ************************************************************************** */
 
@@ -32,13 +32,13 @@ typedef struct s_format
 
 int			ft_printf(const char *s, ...);
 int			ft_putchar_fd(char c, int fd);
-int			ft_putstr_fd(char *s, int fd);
+int			ft_putstr_fd(const char *s, int fd);
 size_t		ft_strlen(const char *s);
 
 const char	*ft_parse_format(const char *s, t_format *f);
 int			ft_isdigit(int c);
 int			ft_pad(int width, int len, int zero);
-int			ft_putstr_case(char *s, char spec);
+int			ft_putstr_case(const char *s, char spec);
 int			ft_putnbr_base(unsigned long long n, char spec);
 int			ft_putnbr_ptr(unsigned long long n);
 
@@ -47,5 +47,6 @@ int			ft_print_str(t_format f, va_list args);
 int			ft_print_nbr(t_format f, va_list args);
 int			ft_print_hex(t_format f, va_list args);
 int			ft_print_ptr(t_format f, va_list args);
+int			ft_print_percent(t_format f);
 
 #endif
